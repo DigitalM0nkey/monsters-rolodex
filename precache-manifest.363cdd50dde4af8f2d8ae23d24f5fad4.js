21dab5ae0614aa33c2933a98ded9f29d284c1b45
Terrence Kuiper <github@terrencekuiper.com>
2020-08-13 20:53:27 -0700

    Updates

---
+++ precache-manifest.363cdd50dde4af8f2d8ae23d24f5fad4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00e489dbed567a2f06e190fd70272aaf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b87a8df7cec719097980",
+    "url": "/monsters-rolodex/static/css/main.b522c268.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b87a8df7cec719097980",
+    "url": "/monsters-rolodex/static/js/main.9d439095.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
